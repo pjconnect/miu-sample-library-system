684c5d17567ab690dda5743097c71710fd7ffb7f
Ponchanon Datta Rone <pronecse@gmail.com>
2023-04-17 09:25:01 -0500

    Test Data Updated

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # MIUMPPLibrarySystem
+
+######. Group 01 ######
+###-----------------###
+
+#Admin Users
+# U: james , P: abc123
+
+#Librarian Users
+# U: lib1 , P: abc123
+# U: lib2 , P: abc123
+# U: iqbal , P: abc123
+# U: kafi , P: abc123
+
+# Admin+Librarian Users
+# U: pj , P: abc123
+# U: ponchanon , P: abc123
+# U: joanna , P: abc123
+
+
+#### First page of the software is the LoginUI
--- src/data/TestData.java
+++ src/data/TestData.java
@@ -9,9 +9,13 @@ public class TestData {
     public static List<User> getTestUsers(){
         var tmpUsers = new ArrayList<User>();
         tmpUsers.add(new User("james" ,"abc123", "James", "Torantneeno", UserRoles.Admin));
+        tmpUsers.add(new User("pj" ,"abc123", "Pasindu", "J", UserRoles.Both));
+        tmpUsers.add(new User("ponchanon" ,"abc123", "Ponchanon", "Rone", UserRoles.Both));
+        tmpUsers.add(new User("lib1" ,"abc123", "Library", "01", UserRoles.Librarian));
+        tmpUsers.add(new User("lib2" ,"abc123", "Library", "02", UserRoles.Librarian));
+        tmpUsers.add(new User("iqbal" ,"abc123", "Md", "Iqbal", UserRoles.Librarian));
         tmpUsers.add(new User("kafi" ,"abc123", "Kafi", "Gordan", UserRoles.Librarian));
         tmpUsers.add(new User("joanna" ,"abc123", "Joanna", "Saleem", UserRoles.Both));
-        tmpUsers.add(new User("pj" ,"abc123", "Pasindu", "J", UserRoles.Both));
 
         return tmpUsers;
     }
@@ -19,6 +23,7 @@ public class TestData {
     public static List<Address> getAddresses(){
         var tmpAddresses = new ArrayList<Address>();
         tmpAddresses.add(new Address("1000 n", "4th st", "IA", "52556"));
+        tmpAddresses.add(new Address("100 n", "main st", "IA", "52557"));
         tmpAddresses.add(new Address("58", "4th st", "Randolph, MA", "02368"));
         tmpAddresses.add(new Address("7361", "Ann Street", "Mundelein, IL", "60060"));
         tmpAddresses.add(new Address("30", "Orange Street", "Union, NJ", "07083"));
@@ -44,9 +49,13 @@ public class TestData {
     public static List<Member> getMembers() {
         var tmpMembers = new ArrayList<Member>();
         tmpMembers.add(new Member(1, "James", "Gordan", "3343 4343", getAddresses().get(1)));
-        tmpMembers.add(new Member(2, "Abigail", "Claire", "2347 4343", getAddresses().get(1)));
-        tmpMembers.add(new Member(3, "Alexander", "Cameron", "8876 4343", getAddresses().get(1)));
-        tmpMembers.add(new Member(4, "Christopher", "Gordan", "23345 4343", getAddresses().get(1)));
+        tmpMembers.add(new Member(2, "Pasindu", "j", "1426 9765", getAddresses().get(1)));
+        tmpMembers.add(new Member(3, "Ponchanon", "Rone", "1426 9645", getAddresses().get(1)));
+        tmpMembers.add(new Member(4, "Library", " 01", "1326 9545", getAddresses().get(1)));
+        tmpMembers.add(new Member(5, "Library", "02", "1316 9645", getAddresses().get(1)));
+        tmpMembers.add(new Member(6, "Md", "Iqbal", "1325 3645", getAddresses().get(1)));
+        tmpMembers.add(new Member(7, "kafi", "Gordan", "1326 9645", getAddresses().get(1)));
+        tmpMembers.add(new Member(8, "Joanna", "Saleem", "1726 9645", getAddresses().get(1)));
         return tmpMembers;
     }
 }
